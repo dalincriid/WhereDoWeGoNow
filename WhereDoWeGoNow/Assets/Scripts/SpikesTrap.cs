a22d34cd626b9226cf04775a32c8f8ef0a894b6a
Paul-Emile Le Reun <paulemile.lereun@gmail.com>
2015-01-24 03:22:15 +0100

    spikes trap

---
+++ WhereDoWeGoNow/Assets/Scripts/SpikesTrap.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesTrap : MonoBehaviour
+{
+	public float Interval = 2.0f;
+
+	private float interval;
+	private Transform[] childrenTransforms;
+
+	// Use this for initialization
+	void Start()
+	{
+		this.childrenTransforms = this.GetComponentsInChildren<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.interval += Time.deltaTime;
+
+		if (this.interval >= this.Interval)
+		{
+			foreach (var item in this.childrenTransforms)
+			{
+				item.position = new Vector3(item.position.x, -item.position.y, item.position.z);
+			}
+			this.interval = 0.0f;
+		}
+	}
+}
