cafa661519f557e0f1ea672fb7a658cfeae6e441
mossad_s <s.mossaddek@student.isartdigital.com>
2015-01-25 14:48:22 +0100

    SALOPE

--- WhereDoWeGoNow/Assets/Scripts/SpikesTrap.cs
+++ WhereDoWeGoNow/Assets/Scripts/SpikesTrap.cs
@@ -19,6 +19,21 @@ public class SpikesTrap : MonoBehaviour
 		}
 	}
 
+    protected void OnSerializeNetworkView(BitStream stream, NetworkMessageInfo info)
+    {
+        Vector3 syncPosition = Vector3.zero;
+        if (stream.isWriting)
+        {
+            syncPosition = rigidbody.position;
+            stream.Serialize(ref syncPosition);
+        }
+        else
+        {
+            stream.Serialize(ref syncPosition);
+            rigidbody.position = syncPosition;
+        }
+    }
+
     protected void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
